033e848428d4c767c6cc537bdfef29b9343e1db1
isseium <issei@ikmz.net>
2016-07-28 15:04:24 +0900

    ignore build dir

--- iphone/build/TiGrowthbeat.build/Release-iphonesimulator/TiGrowthbeat.build/Objects-normal/i386/NetIkmzTigrowthbeatModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/issei/repos/ti/TiGrowthbeat/iphone/Classes/NetIkmzTigrowthbeatModuleAssets.m \
-  /Users/issei/repos/ti/TiGrowthbeat/iphone/Classes/NetIkmzTigrowthbeatModuleAssets.h
